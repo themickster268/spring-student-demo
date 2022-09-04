1f960690c0aa6266a6514030213a469b578d7503
michael.donnelly <themickster268@gmail.com>
2022-09-04 23:07:38 +0100

    first commit

---
+++ src/main/java/com/example/springstudentdemo/SpringStudentDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.springstudentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudentDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudentDemoApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/example/springstudentdemo/SpringStudentDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springstudentdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudentDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
